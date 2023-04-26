d7e2f59d96812cf81c2f10940037d712d1b51add
Oliver Randall <ojrandall@outlook.com>
2023-04-26 21:42:54 +0100

    rubocop

--- app/models/meal_report.rb
+++ app/models/meal_report.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class MealReport < ApplicationRecord
   has_many :meals
 end
--- db/migrate/20230426195845_create_meal.rb
+++ db/migrate/20230426195845_create_meal.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class CreateMeal < ActiveRecord::Migration[7.0]
   def change
     create_table :meals do |t|
