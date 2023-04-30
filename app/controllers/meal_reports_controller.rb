a110b47ad5f7e35d81ded4a28f6802ddf79694d3
Oliver Randall <ojrandall@outlook.com>
2023-04-30 18:45:03 +0100

    fix meal report controller rspec

--- app/controllers/meal_reports_controller.rb
+++ app/controllers/meal_reports_controller.rb
@@ -2,10 +2,7 @@
 
 class MealReportsController < ApplicationController
   def index
-    # Scope your query to the dates being shown:
     start_date = params.fetch(:start_date, Date.today).to_date
     @meal_reports = MealReport.where(report_date: start_date.beginning_of_week..start_date.end_of_week)
-
-    # @meal_reports = MealReport.all
   end
 end
--- spec/controllers/meal_reports_controller_spec.rb
+++ spec/controllers/meal_reports_controller_spec.rb
@@ -2,17 +2,27 @@ require 'rails_helper'
 
 describe MealReportsController do
   describe 'GET index' do
-    subject { get :index }
-
     let(:meal_report) { class_double(MealReport, :meal_report) }
+    let(:date_range) { { report_date: start_date.beginning_of_week..start_date.end_of_week } }
 
     before do
-      allow(MealReport).to receive(:all) { meal_report }
+      allow(MealReport).to receive(:where).with(date_range) { meal_report }
     end
 
-    it 'assigns meal_reports' do
-      subject
-      expect(assigns(:meal_reports)).to eq meal_report
+    context 'when there is no start date param' do
+      let(:start_date) { Date.today }
+      it 'assigns meal_reports using todays date' do
+        get :index
+        expect(assigns(:meal_reports)).to eq meal_report
+      end
+    end
+
+    context 'when there is a start date param' do
+      let(:start_date) { Date.parse('2023-04-10') }
+      it 'assigns meal_reports created in the week specified' do
+        get :index, params: { start_date: '2023-04-10' }
+        expect(assigns(:meal_reports)).to eq meal_report
+      end
     end
   end
 end
