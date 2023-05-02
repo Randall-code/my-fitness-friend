e10e34be6abdc5fe0235d0bce775c5f3f1a4b35d
Randall-code <ojrandall@outlook.com>
2023-05-02 14:00:18 +0100

    add meal create controller feature

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -4,4 +4,19 @@ class MealsController < ApplicationController
   def new
     @meal = Meal.new
   end
+
+  def create
+    @meal = Meal.new(meal_params)
+    if @meal.save
+      redirect_to meal_reports_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def meal_params
+    params.require(:meal).permit(:notes, :meal_report_id, :meal_type )
+  end
 end
--- spec/controllers/meals_controller_spec.rb
+++ spec/controllers/meals_controller_spec.rb
@@ -17,19 +17,41 @@ describe MealsController do
   end
 
   describe 'POST create' do
-    subject { post :create, params: }
+    subject { post :create, params: params}
     let(:params) do
-      {
-        meal_type: 'breakfast',
-        notes: 'This is it'
+      { meal: 
+        {
+          meal_type: 'Breakfast',
+          notes: 'This is it',
+          meal_report_id: 1
+        }
       }
-
+    end
+  
+    context "when the meal is valid" do
+      before do
+        create(:meal_report)
+      end
       it 'creates a meal' do
-        subject
-        meal = Meal.first
+        post :create, params: params
         expect(Meal.count).to eq 1
-        expect(mmeal.notes).to eq('This is it')
-        expect(mmeal.meal_type).to eq('breakfast')
+        expect(assigns(:meal).notes).to eq('This is it')
+        expect(assigns(:meal).meal_type).to eq('Breakfast')
+      end
+
+      it "redirects to the meal report index page" do
+        expect(subject).to redirect_to(meal_reports_path)
+      end
+    end
+
+    context "when the meal is not valid" do
+      before do 
+        allow(Meal).to receive(:create) {nil}
+      end
+
+      it "renders the new meal page" do
+        subject
+        expect(response).to render_template(:new)
       end
     end
   end
